8664b1864f8b153cb77d61ffab1182773ea515ab
al-jabir <aljabir098@gmail.com>
2022-10-18 00:53:00 +0600

    I have finish header components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Header.css';
+
+const nav_list = [
+  {
+    path: '#home',
+    display: 'Home',
+  },
+  {
+    path: '#about',
+    display: 'About',
+  },
+  {
+    path: '#service',
+    display: 'Service',
+  },
+  {
+    path: '#projects',
+    display: 'Projects',
+  },
+  {
+    path: '#blog',
+    display: 'Blog',
+  },
+];
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='container'>
+        <div className='nav_wrapper'>
+          <div className='logo'>
+            <h2>Agency</h2>
+          </div>
+
+          <div className='navigation'>
+            <ul className='menu'>
+              {nav_list.map((item, inx) => (
+                <li className='menu_item'>
+                  <a href={item.path} className='link'>
+                    {item.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='light_mode'>
+            <span>
+              <i class='ri-sun-line'></i> Light Mode
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
