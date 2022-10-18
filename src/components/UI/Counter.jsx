cd56f89893a62edc0583348494dc7b761c423665
al-jabir <aljabir098@gmail.com>
2022-10-18 07:30:58 +0600

    I have complete counter component and style

---
+++ src/components/UI/Counter.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../../styles/Counter.css';
+
+const counterData = [
+  {
+    id: 1,
+    number: '5K',
+    text: 'clients',
+  },
+  {
+    id: 2,
+    number: 350,
+    text: 'Running Projects',
+  },
+  {
+    id: 3,
+    number: 590,
+    text: 'Projects Complete',
+  },
+];
+
+const Counter = () => {
+  return (
+    <section className='counter'>
+      <div className='container'>
+        <div className='counter-wrapper'>
+          {counterData.map(({ number, text, id }) => (
+            <div className='counter-item' key={id}>
+              <h3 className='counter-number'>{number}</h3>
+              <h4 className='counter-title'>{text}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
